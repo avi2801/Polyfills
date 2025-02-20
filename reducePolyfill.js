a1be3e44b81fd3ed6383fc802a5e4ce9b69ea9fa
HP <avinashtiwary6300@gmail.com>
2025-02-20 17:56:31 +0530

    added the polyfill for the reduce function in javascript

---
+++ reducePolyfill.js
@@ -0,0 +1,19 @@
+Array.prototype.myReduce = function (callBack, initialValue) {
+	if (typeof callBack !== 'function') {
+		throw new TypeError('Not a callabale function')
+	}
+
+	let acc = initialValue;
+	let startIndex = 0;
+	if (initialValue === undefined) {
+		if (this.length === 0) {
+			throw new Error('Not an array which was use to call this')
+		}
+		acc = this[0];
+		startIndex= 1
+	}
+	for (let i = 0; i < this.length; i++){
+		acc = callBack.call(acc,this[i],i,this)
+	}
+	return acc;
+}
